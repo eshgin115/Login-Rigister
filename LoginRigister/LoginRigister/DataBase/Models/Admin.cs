41289e701610bf93c979b88b33d03c80be8175ad
unknown <you@example.com>
2022-07-24 06:04:03 -0700

    Created new  child class Admin

---
+++ LoginRigister/LoginRigister/DataBase/Models/Admin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginRigister.DataBase.Models
+{
+    class Admin : User
+    {
+        public Admin(string name, string lastName, string email, string password, int id, DateTime time, int number)
+            : base(name, lastName, email, password, id, time, number)
+        {
+
+        }
+        public Admin(string name, string lastName, string email, string password)
+            : base(name, lastName, email, password)
+        {
+
+        }
+        public override string GetInfo()
+        {
+            return Name + " " + LastName + " " + Email + " " + Id + " " + _rigisterTime;
+        }
+    }
+}
