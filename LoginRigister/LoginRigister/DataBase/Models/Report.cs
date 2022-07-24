ba4e4d12f5af936c910cb410044121a2780b493a
unknown <you@example.com>
2022-07-24 06:25:20 -0700

    Create class Report

---
+++ LoginRigister/LoginRigister/DataBase/Models/Report.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginRigister.DataBase.Models
+{
+    internal class Report
+    {
+    }
+}
--- LoginRigister/LoginRigister/DataBase/Models/User.cs
+++ LoginRigister/LoginRigister/DataBase/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LoginRigister.DataBase.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,26 +9,45 @@ namespace LoginRigister.DataBase.Models
 {
     class User
     {
-        private static int _idcounter = 1;
+
         public int Id { get; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public DateTime _rigisterTime { get; set; }
+        public DateTime _rigisterTime { get; }
+        public int NumberOnTheList { get; }
+
+        public User(string name, string lastName, string email, string password, int id, DateTime time, int number)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            _rigisterTime = time;
+            NumberOnTheList = number;
+        }
         public User(string name, string lastName, string email, string password)
         {
-            Id = _idcounter;
-            _idcounter++;
+
+
             Name = name;
             LastName = lastName;
             Email = email;
             Password = password;
             _rigisterTime = DateTime.Now;
+            
+            Id = UserRepo.IDCounter;
+            NumberOnTheList = UserRepo.NumberOnTheList;
+
+
         }
-        public string GetInfo()
+
+
+        public virtual string GetInfo()
         {
-            return Name + " " + LastName + " " + Email+" "+Id;
+            return Name + " " + LastName + " " + _rigisterTime;
         }
 
     }
