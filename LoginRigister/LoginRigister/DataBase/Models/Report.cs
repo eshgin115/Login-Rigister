51c0cb6ee9eb9b13afdca61da20c91c00e253bb5
unknown <you@example.com>
2022-07-24 07:12:46 -0700

    Update Report class

--- LoginRigister/LoginRigister/DataBase/Models/Report.cs
+++ LoginRigister/LoginRigister/DataBase/Models/Report.cs
@@ -9,8 +9,22 @@ namespace LoginRigister.DataBase.Models
     class Report
     {
 
-        public string reporterEmail { get; set; }
-        public string text { get; set; }
-        public static int id { get; set; }
+        public static int _numberOnTheList = 1;
+        public int NumberOnTheList { get; set; }
+        public string  FromMSS { get; set; }
+        public string ToMSS { get; set; }
+        public string Text { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime ReportTIME { get; set; }
+        public Report(int numberOnTheList, string fromMSS, string toMSS, string text, bool isAdmin, DateTime reportTIME)
+        {
+          
+            FromMSS = fromMSS;
+            ToMSS = toMSS;
+            Text = text;
+            IsAdmin = isAdmin;
+            ReportTIME = reportTIME;
+        }
+      
     }
 }
